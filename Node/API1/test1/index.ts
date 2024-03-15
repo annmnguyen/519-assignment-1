bde08d8f3a55e51d3a71a008eb794984d8575874
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-15 19:06:30 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -4,6 +4,17 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
+    const keyvaultname = 'assignment-4'
+    const credential = new ManagedIdentityCredential();
+    const secretname = 'secret2'
+    const secretclient = new secretclient(keyvaultname, credential)
+    const secret = await secretClient.getSecret(secretname)
+    const vault = 'https://assignment-4.vault.azure.net/'
+        if (secret) {
+            context.log(`Retrieved secret value: ${secret.value}`);
+        } else {
+            context.log(`Unable to retrieve secret.`);
+        }
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
