cc5609fd91a47d11938a37c916cd1ccfbb5aa6f0
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-15 18:39:40 -0400

    Update index.js

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 app.use('/', express.static('frontend/build'));
 
 app.get('/api', (req, res) => {
-  const secretValue = process.env.secret3;  
+  const secretValue = process.env.myAppSetting;  
   res.json({ message: 'Hello, world!', secret: secretValue || 'No secret found' });
 });
 
