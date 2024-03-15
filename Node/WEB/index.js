87a7ba3bdbedec6a52bc0a61d7dff3a05f936b78
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-15 17:36:47 -0400

    add secret

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000
 app.use('/', express.static('frontend/build'));
 
 app.get('/api', (req, res) => {
-  res.send('Hello, world!');
+  const secretValue = process.env.CocoSecret;  
+  res.json({ message: 'Hello, world!', secret: secretValue || 'No secret found' });
 });
 
 app.listen(port, () => {
