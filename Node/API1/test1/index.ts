8ba5995c84f2c65a5b8adcc868d9f6bc85451e18
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-15 19:23:38 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -1,49 +1,17 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import { DefaultAzureCredential, ManagedIdentityCredential } from "@azure/identity";
-import { SecretClient } from "@azure/keyvault-secrets";
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-
-    // Get the name from query parameters or request body
     const name = (req.query.name || (req.body && req.body.name));
-
-    try {
-        // Initialize the SecretClient using Managed Identity
-        const keyVaultName = "assignment-4";
-        const vaultUri = `https://${'assignment-4'}.vault.azure.net`;
-        const credential = new ManagedIdentityCredential();
-
-        // Access your secret from Key Vault
-        const secretName = "secret2";
-        const secretClient = new SecretClient(vaultUri, credential);
-
-        // Retrieve the secret value
-        const secret = await secretClient.getSecret(secretName);
-
-        // Log the secret value
-        if (secret) {
-            context.log(`Retrieved secret value: ${secret.value}`);
-        } else {
-            context.log(`Unable to retrieve secret.`);
-        }
-
-        // Prepare the response message
-        const responseMessage = name
-            ? `Hello, ${name}. This HTTP triggered function executed successfully.`
-            : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-        // Set the response
-        context.res = {
-            body: responseMessage
-        };
-    } catch (error) {
-        context.log.error("Error accessing secret from Key Vault:", error);
-        context.res = {
-            status: 500,
-            body: "Internal Server Error"
-        };
-    }
+    const d = new Date()
+    const responseMessage = name
+    const secret = process.env['secret2'];
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully.Today's date is " + d + "."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+    context.log("This is secret " + secret + ".");
 };
-
 export default httpTrigger;
