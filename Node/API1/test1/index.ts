e64dcde9b2465675e2111c524bd5750aaa671efe
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-15 20:40:26 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -5,9 +5,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const name = (req.query.name || (req.body && req.body.name));
     const d = new Date()
     const responseMessage = name
-    context.log("assignment-4" + process.env["assignment-4"]);
-    context.log("myAppSetting" + process.env["myAppSetting"]);
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully.Today's date is " + d + "."
+    const secret = process.env.myAppSettings;
+    context.log("Secret: " + secret);
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully.Today's date is " + d + secret + "."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     context.res = {
         // status: 200, /* Defaults to 200 */
