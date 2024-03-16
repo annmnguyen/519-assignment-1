b411d4be77a32744189ce8416bdc283856e52c68
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-15 23:33:35 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -6,7 +6,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log(`Secret = ${secretValue}`);
     const name = (req.query.name || (req.body && req.body.name));
     const d = new Date()
-    const responseMessage = name
+    const queueMessage = "Queue added";
+    context.bindings.outputQueueItem.queueMessage;
+    const responseMessage = "item added to queue"
         ? "Hello, " + name + ". This HTTP triggered function executed successfully.Today's date is " + d + "."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     context.res = {
