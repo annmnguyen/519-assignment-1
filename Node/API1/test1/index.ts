476f43dbfcfc79d5c9dd7734aa6cc8a78e4cee5b
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-15 20:47:08 -0400

    Update index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -2,12 +2,12 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+    const secretValue = process.env.myAppSetting;
+    context.log(`Secret = ${secretValue}`);
     const name = (req.query.name || (req.body && req.body.name));
     const d = new Date()
     const responseMessage = name
-    const secret = process.env.myAppSettings;
-    context.log("Secret: " + secret);
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully.Today's date is " + d + secret + "."
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully.Today's date is " + d + "."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     context.res = {
         // status: 200, /* Defaults to 200 */
