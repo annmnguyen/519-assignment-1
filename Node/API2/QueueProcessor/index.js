22cdfe22a5d2bd4aa320452d14eee40addaca805
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-03-23 18:44:26 -0400

    Update index.js

--- Node/API2/QueueProcessor/index.js
+++ Node/API2/QueueProcessor/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    const secret3 = process.env.myStorageConnectionAppSetting;
+    const secret3 = process.env.mystorageconnectionappsetting;
     context.log('secret 3:', secret3);
 };
