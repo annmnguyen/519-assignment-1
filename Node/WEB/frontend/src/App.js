0b8110f756fb30bbd36a44f194b2cdaf3b57dbc7
annmnguyen <101607810+annmnguyen@users.noreply.github.com>
2024-04-23 00:30:21 -0400

    Update App.js

--- Node/WEB/frontend/src/App.js
+++ Node/WEB/frontend/src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React { UseEffect, UseState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const handleUpload = (evemt) => {
+    const files = event.target.files;
+    const filedata = new FileData();
+    Array.from(files).forEach(file => fileData.append('photos', file));
+    axios.post('/api/UploadFile', filedata)
+      .then(response => {
+        console.log('upload success', reponse.data);
+      })
+      .catch(error => {
+        console.error('upload failed', error);
+      });
+  };
+    
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="file" multiple onChange={handleUpload} />
         <a
           className="App-link"
           href="https://reactjs.org"
