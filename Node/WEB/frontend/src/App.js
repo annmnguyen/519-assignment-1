416cf0fff2b8fe4e2c9eee721d2c11dfea6d7331
annmnguyen <nhnguyen@bu.edu>
2024-04-23 14:53:25 -0400

    reverse changes to restart web app

--- Node/WEB/frontend/src/App.js
+++ Node/WEB/frontend/src/App.js
@@ -1,21 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import axios from 'axios';
 
 function App() {
-  const handleUpload = (event) => {
-    const files = event.target.files;
-    const filedata = new FormData();
-    Array.from(files).forEach(file => filedata.append('photos', file));
-    axios.post('/api/UploadFile', filedata)
-      .then(response => {
-        console.log('upload success', response.data);
-      })
-      .catch(error => {
-        console.error('upload failed', error);
-      });
-  };
     
   return (
     <div className="App">
@@ -24,7 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="file" multiple onChange={handleUpload} />
         <a
           className="App-link"
           href="https://reactjs.org"
