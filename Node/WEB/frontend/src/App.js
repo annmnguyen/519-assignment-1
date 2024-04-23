60a2c7eea13a40ce6dc4a115d679c4508f18b338
annmnguyen <nhnguyen@bu.edu>
2024-04-23 19:59:30 -0400

    testing app.js file

--- Node/WEB/frontend/src/App.js
+++ Node/WEB/frontend/src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <button> hi</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
