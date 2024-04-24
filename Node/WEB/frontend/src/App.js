b070ace00cf5e75b4781ee3b74774f31df795d3c
annmnguyen <nhnguyen@bu.edu>
2024-04-23 20:22:25 -0400

    testing out upload file and api

--- Node/WEB/frontend/src/App.js
+++ Node/WEB/frontend/src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    
+  const handleUpload = (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    document.getElementById('fileUploadForm').submit(); // Submit the form
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -11,9 +14,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         
-        <div>
-          <button style={{ background: "#801212"}} onClick={handleUpload}>Upload</button>
-        </div>
+        <form id="fileUploadForm" action="http://localhost:3000/analyze" method="post" enctype="multipart/form-data">
+          <input type="file" name="file" required />
+          <button type="button" onClick={handleUpload} style={{ background: "#801212" }}>
+            Upload Image
+          </button>
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
